5f54ae92ac4ce3d0eb434ba522995e89a1c1e650
Anna <panna.anna101@gmail.com>
2015-06-09 20:55:50 +0200

    Teksty.org class

--- src/thevoice/Artist.java
+++ src/thevoice/Artist.java
@@ -3,10 +3,6 @@ package thevoice;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 /**
  *
@@ -34,34 +30,9 @@ public class Artist {
     }
 
     public void addTextsFromTekstyorg() throws IOException {
-        
-        String newName = name.toLowerCase().replace(" ", "-");
-        //String link = "http://www.azlyrics.com/" + newName.charAt(0) + "/" + newName + ".html";
-        String link = "http://teksty.org/" + newName + ",teksty-piosenek/";
-        System.out.println(link);
-        String last = Jsoup.connect(link).get().select("a[title=Ostatnia strona]").text();
-        int lastPage;
-        if ("".equals(last))
-            lastPage = 0;
-        else lastPage = Integer.parseInt(last.substring(4));
-        
-            
-        for (int i = 0; i <= lastPage; i++) {
-            String pageLink = link + i;
-            //System.out.println(pageLink);
-            Document d = Jsoup.connect(pageLink).get();
-            Elements links = d.select("div.artistSongs").select("a.artist");
-            for (Element el: links) {
-                if (el.attr("href").endsWith("tekst-piosenki")) {
-                    System.out.println(el.attr("href"));
-                    Document page = Jsoup.connect(el.attr("href")).get();
-                    String text = page.select("div.originalText").get(0).text();
-                    textsWords.addFromString(text);
-                }
-            }
-            //System.out.println(links);
-        }
-        
+        String[] songLinks = TekstyOrg.getSongsLinks(name);
+        for (String link: songLinks)        
+            textsWords.addFromString(TekstyOrg.getSongStringFromLink(link));
     }
     
     public List top5(WordsList filters) {
